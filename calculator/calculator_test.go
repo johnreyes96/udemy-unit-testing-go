92c2ef9a648c3c2243b201197ae0e567fcbe5507
John Reyes <jhonfer96@hotmail.com>
2023-02-10 00:29:11 -0500

    section two: refactoring improving message failed tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -20,7 +20,6 @@ func TestDiscountNotApplied(t *testing.T) {
 	t.Log("Hello!")
 
 	if 50 != amount {
-		t.Logf("expected 50, got %v", amount)
-		t.Fail()
+		t.Errorf("expected 50, got %v, failed because the discount was not expected to be applied", amount) // Error = Log + Fail
 	}
 }
