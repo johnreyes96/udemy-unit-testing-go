21c1510785bc1a607289c69ea2c0d1b557c7a5f8
John Reyes <jhonfer96@hotmail.com>
2023-02-10 16:42:17 -0500

    section two: improving tests with sub tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestDiscountCalculator(t *testing.T) {
 	type testCase struct {
+		name                  string
 		minimumPurchaseAmount int
 		discount              int
 		purchaseAmount        int
@@ -11,18 +12,21 @@ func TestDiscountCalculator(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmount: 130},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 200, expectedAmount: 160},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmount: 290},
-		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmount: 50},
+		{name: "should apply 20", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmount: 130},
+		{name: "should apply 40", minimumPurchaseAmount: 100, discount: 10, purchaseAmount: 200, expectedAmount: 160},
+		{name: "should apply 60", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmount: 290},
+		{name: "should not apply", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmount: 50},
 	}
 
 	for _, tc := range testCases {
-		calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
-		amount := calculator.Calculate(tc.purchaseAmount)
+		t.Run(tc.name, func(t *testing.T) {
+			calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
 
-		if amount != tc.expectedAmount {
-			t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
-		}
+			amount := calculator.Calculate(tc.purchaseAmount)
+
+			if amount != tc.expectedAmount {
+				t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
+			}
+		})
 	}
 }
