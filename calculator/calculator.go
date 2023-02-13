ce592c5ba4e8ba4670cdd2182fba3541cf8fb83b
John Reyes <jhonfer96@hotmail.com>
2023-02-12 22:24:06 -0500

    section two: testing with fatal methods

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,15 +1,21 @@
 package calculator
 
+import "errors"
+
 type DiscountCalculator struct {
 	minimumPurchaseAmount int
 	discountAmount        int
 }
 
-func NewDiscountCalculator(minimumPurchaseAmount, discountAmount int) *DiscountCalculator {
+func NewDiscountCalculator(minimumPurchaseAmount, discountAmount int) (*DiscountCalculator, error) {
+	if minimumPurchaseAmount == 0 {
+		return &DiscountCalculator{}, errors.New("minimum purchase amount could not bet zero")
+	}
+
 	return &DiscountCalculator{
 		minimumPurchaseAmount: minimumPurchaseAmount,
 		discountAmount:        discountAmount,
-	}
+	}, nil
 }
 
 func (c DiscountCalculator) Calculate(purchaseAmount int) int {
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -13,14 +13,19 @@ func TestDiscountCalculator(t *testing.T) {
 
 	testCases := []testCase{
 		{name: "should apply 20", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmount: 130},
-		{name: "should apply 40", minimumPurchaseAmount: 100, discount: 10, purchaseAmount: 200, expectedAmount: 160},
+		{name: "should apply 40", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 200, expectedAmount: 160},
 		{name: "should apply 60", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmount: 290},
 		{name: "should not apply", minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmount: 50},
+		{name: "zero", minimumPurchaseAmount: 0, discount: 20, purchaseAmount: 50, expectedAmount: 50},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
+			calculator, err := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
+			if err != nil {
+				// Fail + log
+				t.Errorf("could not instantiate the calculator %s", err.Error())
+			}
 
 			amount := calculator.Calculate(tc.purchaseAmount)
 
