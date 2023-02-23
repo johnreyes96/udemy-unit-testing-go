bddfca6552bcffcea53614f0edfba3dd5d15bf78
John Reyes <jhonfer96@hotmail.com>
2023-02-23 00:10:56 -0500

    section three: add mock using mock.Mock

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -4,19 +4,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 type DiscountRepositoryMock struct {
-	Counter int
+	mock.Mock
 }
 
 func (drm DiscountRepositoryMock) FindCurrentDiscount() int {
-	drm.Counter++
-
-	if drm.Counter <= 4 {
-		return 20
-	}
-	return 0
+	args := drm.Called()
+	return args.Int(0)
 }
 
 func TestDiscountCalculator(t *testing.T) {
@@ -24,6 +21,7 @@ func TestDiscountCalculator(t *testing.T) {
 		name                  string
 		minimumPurchaseAmount int
 		purchaseAmount        int
+		discount              int
 		expectedAmount        int
 	}
 
@@ -32,30 +30,35 @@ func TestDiscountCalculator(t *testing.T) {
 			name:                  "should apply 20",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        150,
+			discount:              20,
 			expectedAmount:        130,
 		},
 		{
 			name:                  "should apply 40",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        200,
+			discount:              20,
 			expectedAmount:        160,
 		},
 		{
 			name:                  "should apply 60",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        350,
+			discount:              20,
 			expectedAmount:        290,
 		},
 		{
 			name:                  "should not apply",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
+			discount:              20,
 			expectedAmount:        50,
 		},
 		{
 			name:                  "should not apply when discount is zero",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
+			discount:              0,
 			expectedAmount:        50,
 		},
 	}
@@ -63,6 +66,7 @@ func TestDiscountCalculator(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			discountRepositoryMock := DiscountRepositoryMock{}
+			discountRepositoryMock.On("FindCurrentDiscount").Return(tc.discount)
 			calculator, err := NewDiscountCalculator(tc.minimumPurchaseAmount, discountRepositoryMock)
 			if err != nil {
 				// FailNow + log
